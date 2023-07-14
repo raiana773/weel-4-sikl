89ecb69a1e63c1199b47e9dad6ba6cbf1a9b0478
raiana773 <104357057+raiana773@users.noreply.github.com>
2023-07-15 03:30:39 +0545

    sikl

---
+++ js/main.js
@@ -0,0 +1,18 @@
+// for, while, do..while, for..in, for..of
+
+
+// for
+// let arr = [1, 2, 3, 4, 5];
+// // for(period-период){действие или же тело цикла body/action}
+// for(let i = 0; i < arr.length; i++){
+//     console.log(arr [i] * 2);
+// }
+
+
+let data = ['Kubat', 'Aktan', 'Kani', 'Adilet'];
+for (let i = 0; i < data.length; i++){
+    if(data[i]=== 'Aktan'){
+        console.log(`${data[i]} `)
+    }
+    console.log(`${data[i]} you are invited`);
+}
